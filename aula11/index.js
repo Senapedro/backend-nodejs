a9981a8915a613e9636fc5b8697d1747440feeec
pedro sena <ph997595305@gmail.com>
2023-03-28 20:12:06 -0300

    Commit

--- aula11/index.js
+++ aula11/index.js
@@ -1,6 +1,8 @@
-const json = '{"nome": "Pedro","idade": 25,"professor": true,"turmas":[10,12,15,16],"endereco":{"rua"}:"teste","numero":123,"bairro":"vila","cep":"123.123-12"}'
+const jsonEntrada = '{"nome":"Pedro","idade": 25,"professor": true,"turmas":[10,12,15,16],"endereco":{"rua"}:"teste","numero":123,"bairro":"vila","cep":"123.123-12"}'
 
 const obj = JSON.parse(jsonEntrada);
 
 console.log(obj);
+
+
 
