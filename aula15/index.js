9726a456988e4ad24e4b1c28c4229d4f9e5fa4f8
pedro sena <ph997595305@gmail.com>
2023-04-05 15:00:48 -0300

    atualização

--- aula15/index.js
+++ aula15/index.js
@@ -1,13 +1,32 @@
 const express = require("express");
-
 const app = express();
+const request = require("request");
 
 const port = 3000;
 
 app.use(express.json()); 
 
+
 app.get("/", (req,res) => {
-    res.send("Hello Word");
+    const url = req.body.url;
+
+    request(url, function (error, response, body) {
+        console.log('statusCode:', response && response.statusCode);
+
+        const pokemon = JSON.parse(body);
+        
+        res.send(pokemon);
+
+        console.log(pokemon.id);
+        console.log(pokemon.name);
+        console.log(pokemon.height);
+        console.log(pokemon.weight);
+        console.log(pokemon.types);
+    });
+})
+
+app.get("/contato", (req,res) => {
+    res.send("Nosso Contato");
 })
 
 app.listen(port, () => {
